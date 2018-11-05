837f3fca0d247b3e368e62f5505372312aeaf947
Nicolas Antontio Escobar Videla <nescobar2016@alu.uct.cl>
2018-11-05 14:47:28 -0300

    guardarhabitacion1.1

--- guardarhabitacion.php
+++ guardarhabitacion.php
@@ -1,4 +1,5 @@
 <?php 
+<<<<<<< Updated upstream
 		$lugar = $_POST["lugar"];//son todos los datos recibidos del formulario de crear un evento
 		$habitacion = $_POST["habitacion"];
 		$personas = $_POST["personas"];
@@ -12,7 +13,4 @@
 		mysql_select_db("hotel");
 		if(!$db) echo "Error en la conexion";
 		$sql="INSERT INTO reservas(Cuidad,Habitacion,Personas,Estadia,Cama) VALUES ('$lugar','$habitacion','$personas','$estadia','$cama')";//Inserta los registros en las tablas
-		$result = mysqli_query($db, $sql);//se conecta y envia los datos
-		if ($result) echo "Datos recibidos con exito.";
-		else echo "Error al insertar registro.";
-?>
+=======
