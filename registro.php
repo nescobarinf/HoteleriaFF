18fc49bfad3194105b85c29706b5bc2f24cbb6ca
Nicolas Antontio Escobar Videla <nescobar2016@alu.uct.cl>
2018-11-05 14:53:14 -0300

    Revert "registro"
    This reverts commit c8bca4536ddb800867520be1d1d08f09f2eb040b.

--- registro.php
+++ registro.php
@@ -1,40 +1,85 @@
-<?php
+<html>
+    
+ <head>
 
-	$nombre=$_POST['nombre'];
-	$apellido=$_POST['apellido'];
-	$rut=$_POST['rut'];
-	$mail=$_POST['correo'];
-	$pass= $_POST['pass'];
-	$rpass=$_POST['rpass'];
+    <meta charset="utf-8">
+    
+    <title>Registro</title>
+    
+    <link rel = "stylesheet" type= "text/css" href= "css/bootstrap.css">
+    
+    
+</head>
 
-	$mysqli = new MySQLi("localhost", "root","", "hotel");
-		if ($mysqli -> connect_errno) {
-			die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
-				. ") " . $mysqli -> mysqli_connect_error());
-		}
-		else
-			//echo "Conexión exitossa!";
+<body>
 
-//	$link =mysqli_connect("localhost","root","");
-//	if($link){
-//		mysqli_select_db($link,"academ");
-//	}
-	$checkemail=mysqli_query($mysqli,"SELECT * FROM usuarios WHERE email='$mail'");
-	$check_mail=mysqli_num_rows($checkemail);
-		if($pass==$rpass){
-			if($check_mail>0){
-				echo ' <script language="javascript">alert("Atencion, ya existe el mail designado para un usuario, verifique sus datos");</script> ';
-			}else{
-				
-				mysqli_query($mysqli,"INSERT INTO usuarios VALUES('','$nombre','$apellido','$rut','$mail','$pass')");
-				//echo 'Se ha registrado con exito';
-				echo ' <script language="javascript">alert("Usuario registrado con éxito");</script> ';
-				
-			}
-			
-		}else{
-			echo 'Las contraseñas son incorrectas';
-		}
+<div class="container">
+    <div class="row">
+        
+    <div class="col-md-5-col-md-offset-3">
+        
+    <div class="page-header text-center">
+        
+        <h1>Registro<small><br>FRIFAIER HOTELS</small></h1>    
+        
+        </div>  
+        <form class="form-signin" method="post" action="pagina.php">
+        
+        <h2 class="form-signing-heading">Unase a nuestra linea de Hoteles</h2>
+        
+        <label for="user" class="sr-only">User</label>
+            
+        <input type="text" name="Usser" class="form-control" placeholder="Username" required autofocus><br><br>
+        
+        <label for="Name" class="sr-only">Name</label>
+        
+        <input type="text" name="Usser" class="form-control" placeholder="Name" required autofocus><br><br>
+        
+        <label for="Apellido" class="sr-only">Apellido</label>
+            
+        <input type="text" name="Usser" class="form-control" placeholder="Apellido" required autofocus><br><br>
+            
+        <label for="Mail" class="sr-only">Email</label>
+        
+        <mail for="text"  mail="MailUsser" class="form-control" placehoder="mail">
+            
+        <input type="text" name="Usser" class="form-control" placeholder="Mail@gmail.com" required autofocus><br><br>
+            
+        <label for="Mail" class="sr-only">Rut</label>
+        
+        <string for="text" name="RUT" class="form-control" placeholder="RUT">
+            
+        <input type="text" name="Usser" class="form-control" placeholder="RUT" required autofocus><br><br>
+           
+        <label for="password" lass="sr-only">Password</label>
+            
+        <input type="password" name="password" class="frorm-control" placeholder="Password" required>
+            
+        <br><br>
+            
+        <label for="password" lass="sr-only">Reescribe Password</label>
+            
+        <input type="password" name="password" class="frorm-control" placeholder="Password" required><br><br>
+                                                                                            
+        M<input type="radio" name="Sex" id="M">
+        
+        F<input type="radio" name="Sex" id="F">
+                                       
+        <br><br>
+            
+        <input type="submit" name="submit" class="btn btn-lg btn-primary btn-block" value="Registro">
+            
+            
+             </string>
+            
+            </mail>
+            
+        </form>
+            </div>
+        </div>
+    
+    </div>
 
-	
-?>
+</body>
+
+</html>
