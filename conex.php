1624256cff424dd55b646850dcb79646cc03002e
Nicolas Antontio Escobar Videla <nescobar2016@alu.uct.cl>
2018-11-19 16:55:41 -0300

    Revert "Contacto v2.0"
    This reverts commit e09704438c314109a24f90b0d29c9abf00e0e127.

--- conex.php
+++ conex.php
@@ -7,7 +7,7 @@
 		$db = mysqli_connect("localhost", "root", "", "hotel");//se conecta a la base de datos
 		mysql_select_db("hotel");
 		if(!$db) echo "Error en la conexion";
-		$sql="INSERT INTO contacto(Nombre,Email,Asunto,Comentario) VALUES ('$Nombre','$Email','$Asunto','$Comentario')";//Inserta los registros en las tablas
+		$sql="INSERT INTO contacto(Nombre,Email,Asunto,Comentario) VALUES ('$Nombre','$Email','$Asunto','Comentario')";//Inserta los registros en las tablas
 		$result = mysqli_query($db, $sql);//se conecta y envia los datos
 		if ($result) echo "Hemos recibido su comentario con exito.";
 		else echo "Error al enviar su consulta, intentelo más tarde.";
