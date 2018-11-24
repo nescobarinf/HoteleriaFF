2f4f2db85390794096f1c9427086c658a2da315a
Nicolas Antontio Escobar Videla <nescobar2016@alu.uct.cl>
2018-11-24 17:05:02 -0300

    loginadmin
    login basico de admins con conexion y pagina para borrar usuarios

---
+++ loginadmin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Admin</title>
+</head>
+<body>
+	<form action="consultaadmin.php" method="post">
+		<label>Ingresar email<input type="email" name="email"></label><br>
+		<label>Ingresar contraseña<input type="password" name="password"></label><br>
+		<input type="submit" name="button" value="Enviar">
+	</form>
+</body>
+</html>
+
+
+<?php
+  ?>
